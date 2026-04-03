d00333608a3fb7e4c74a382b1bfc7e2f839fd609
unknown <sedoriosa.ivan234@gmail.com>
2026-04-03 14:29:13 +0800

    ns1rd

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,16 @@ $router->get('/test-db', function () {
 });
 
 
+$router->get('/env', function() {
+    return [
+        'DB_CONNECTION' => env('DB_CONNECTION'),
+        'DB_HOST' => env('DB_HOST'),
+        'DB_PORT' => env('DB_PORT'),
+        'DB_DATABASE' => env('DB_DATABASE'),
+        'DB_USERNAME' => env('DB_USERNAME'),
+        'DB_PASSWORD' => env('DB_PASSWORD') ? 'set' : 'empty'
+    ];
+});
 
 
 
